c0daeb4a6cfcdbf177fd1fd07e5115e081c6b677
Vedant Mistry <mistryvedant17@gmail.com>
2022-08-07 23:32:56 +0530

    Still not able to get the score to get updated.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+
+let playerScore = 0;
+let computerScore = 0;
+
 // Gets the move played by the computer.
 function getComputerChoice () {
     let choices = ["r", "p", "s"];
@@ -21,48 +28,61 @@ function playRound (playerSelection, computerSelection) {
 
         // Player wins
         case "rs":
+            playerScore += 1;
             return "You win, Rock beats Scissors";
             break;
         case "pr":
+            playerScore += 1;
             return "You win, Paper beats Rock";
             break;
         case "sp":
+            playerScore += 1;
             return "You win, Scissors beats Paper";
             break;
 
         // Computer Wins
         case "sr":
+            computerScore += 1;
             return "You lose, Rock beats Scissors";
             break;
         case "rp":
+            computerScore += 1;
             return "You lose, Paper beats Rock";
             break;
         case "ps":
+            computerScore += 1;
             return "You lose, Scissors beats Paper";
             break;
     }
 }
+// playerSelection = "r";
+// computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
 
-// Keeps track of the score for the game
-function game (playerSelection, computerSelection) {
+// event listeners for each button.
+rockButton.addEventListener(
+    'click', () => {
+        let playerSelection = 'r';
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
 
-    let move = playerSelection + computerSelection;
-    // Player score for the game
-    let playerScore = 0; 
-    // Computer score for the game
-    let computerScore = 0;
-
-    for (let i=0; i<5; i++) {
-        // Player wins
-        if (move == "rs" || move == "pr" || move == "sp") {
-            playerScore++;
-        }
-        else if (move == "sr" || move == "rp" || move == "ps") {
-            computerScore++;
-        }
-        return playerScore + " " + computerScore;
     }
-}
-playerSelection = "r";
-computerSelection = getComputerChoice();
-console.log(game());
+)
+paperButton.addEventListener(
+    'click', () => {
+        let playerSelection = 'p';
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+)
+scissorsButton.addEventListener(
+    'click', () => {
+        let playerSelection = 's';
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+)
+
+document.getElementById('score-screen').textContent = "can you beat the computer";
+document.getElementById('player-header-score').textContent = playerScore;
+document.getElementById('computer-header-score').textContent = computerScore;
